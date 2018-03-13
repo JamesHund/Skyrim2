d11c598dbae9b7263bcf82ef32a40a44759c18b6
James Hund <jameshundp@gmail.com>
2018-03-13 20:35:54 +0200

    Code organization in main
    Removed most code duplicates, added comments, disabled resizable screen

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -11,16 +11,13 @@ func _initialize():
 	var spawnareas = get_tree().get_nodes_in_group("spawnarea")
 	for SpawnArea in spawnareas:
 		SpawnArea.connect("spawn",self,"_on_SpawnArea_spawn")
-	
-	
-func _on_area2d_area_entered():
-	emit_signal("next_level")
-
+		
 func _on_SpawnArea_spawn(pos, extents,type):
 	
 	var x = rand_range(0, extents.x)
 	var y = rand_range(0, extents.y)
 	var spawnpos = Vector2(pos.x + x, pos.y + y)
 	emit_signal("spawn_entity",type,spawnpos)
+
 	
 
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,24 +1,42 @@
 extends Node2D
 
+#Preload Scenes to Instantiate
 onready var projectile = preload("res://Scenes//Entities//Projectiles//Projectile.tscn")
 onready var level = preload("res://Scenes//Level.tscn")
 onready var enemy = preload("res://Scenes//Entities//Characters//Enemy.tscn")
 onready var player = preload("res://Scenes//Entities//Characters//Player.tscn")
 
+#Fields
 var player_is_alive
 var enemy_list = []
 
+#Basic Functions
 func _ready():
 	$Player.start(Vector2(538, 320))
 	player_is_alive = true
-	var teleporters = get_tree().get_nodes_in_group("teleport")
-	for door in teleporters:
-		door.connect("teleport",self,"_on_door_teleport")
-  $Level.connect("spawn_entity",self,"_on_Level_spawn_entity")
+	$Level.connect("spawn_entity",self,"_on_Level_spawn_entity")
 	
 func _process(delta):
 		pass
 
+func respawn():
+	var new_player = player.instance()
+	new_player.set_name("Player")
+	add_child(new_player)
+	$Player.start(Vector2(64, 64))
+	$Player.connect( "playerdeath", self , "_on_Player_playerdeath")
+	$Player.connect( "shoot", self, "_on_Player_shoot")
+	player_is_alive = true
+	
+func _initialize_teleporters():
+	var teleporters = get_tree().get_nodes_in_group("teleport")
+	for Teleporter in teleporters:
+		Teleporter.connect("teleport",self,"_on_Teleporter_teleport")
+	
+
+#Signal functions
+
+#Shooting Projectiles
 func _on_Player_shoot():
 	var new_projectile = projectile.instance()
 	add_child(new_projectile)
@@ -29,32 +47,16 @@ func _on_Enemy_shoot(instance):
 		var new_projectile = projectile.instance()
 		add_child(new_projectile)
 		new_projectile._initialize($Player.position-instance.position, instance)
-
-
-func _on_Player_playerdeath():
-	player_is_alive = false
-	$RespawnTimer.start()
-
-func _on_SpawnArea_spawn():
-	var new_enemy = enemy.instance()
-	add_child(new_enemy)
-	new_enemy._initialize(Vector2(0,0))
-	
-func respawn():
-	var new_player = player.instance()
-	new_player.set_name("Player")
-	add_child(new_player)
-	$Player.start(Vector2(64, 64))
-	$Player.connect( "playerdeath", self , "_on_Player_playerdeath")
-	$Player.connect( "shoot", self, "_on_Player_shoot")
-	player_is_alive = true
-
-
-func _on_RespawnTimer_timeout():
-	respawn()
-
-
-func _on_door_teleport(level, pos):
+		
+#Initializing entities
+func _on_Level_spawn_entity(type,pos):
+	if type==1:
+		enemy_list.append(enemy.instance())
+		add_child(enemy_list[enemy_list.size()-1])
+		enemy_list[enemy_list.size()-1].connect( "shoot", self, "_on_Enemy_shoot")
+		enemy_list[enemy_list.size()-1].start(pos)
+		
+func _on_Teleporter_teleport(level, pos):
 	for i in get_children():
 		if !i.is_in_group("main"):
 			i.queue_free()
@@ -65,17 +67,16 @@ func _on_door_teleport(level, pos):
 	$Player.position = pos
 	$TeleportTimer.start()
 
-func _on_TeleportTimer_timeout():
-	var teleporters = get_tree().get_nodes_in_group("teleport")
-	for door in teleporters:
-		door.connect("teleport",self,"_on_door_teleport")
 
-func _on_Level_spawn_entity(type,pos):
-	print(type)
-	if type==1:
-		enemy_list.append(enemy.instance())
-		add_child(enemy_list[enemy_list.size()-1])
-		enemy_list[enemy_list.size()-1].connect( "shoot", self, "_on_Enemy_shoot")
-		enemy_list[enemy_list.size()-1].start(pos)
-		print("Enemy Spawned")
-		print(pos)
+func _on_Player_playerdeath():
+	player_is_alive = false
+	$RespawnTimer.start()
+
+#Timers
+func _on_RespawnTimer_timeout():
+	respawn()
+
+
+func _on_TeleportTimer_timeout():
+	_initialize_teleporters()
+
