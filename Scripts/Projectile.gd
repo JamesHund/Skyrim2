20a463642a92c8981ea858f0c5deda1e8ca3c657
James <jamoxploder@gmail.com>
2019-08-31 18:54:43 +0200

    Changed bullets to Kinematic bodies
    Fixes issue with clipping bullets

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 
 var direction
 var source
@@ -17,15 +17,31 @@ func _initialize(dir, src, dmg, spd, sprd):
 	speed = spd
 	if src.is_in_group("players"):
 		group = "players"
+		set_collision_mask_bit(3,true)
+		#set_collision_layer_bit(3,true)
 	else:
+		print("Enemy projectile")
 		group = "enemies"
+		set_collision_mask_bit(2,true)
+		#set_collision_layer_bit(4,true)
 	direction = polar2cartesian(1, cartesian2polar(dir.x,dir.y).y + deg2rad(rand_range(0,sprd)-sprd/2))
 	rotation = cartesian2polar(dir.x,dir.y).y
 	$DecayTimer.start()
 	speedtest = 0
 
-func _process(delta):
-	position += direction*speed*delta
+func _physics_process(delta):
+	var collision = move_and_collide(direction*speed*delta)
+	if collision:
+		if collision.collider.is_in_group("characters"):
+			if !collision.collider.is_in_group(group):
+				collision.collider.damage(damage)
+				if collision.collider.is_in_group("enemies"):
+					collision.collider._apply_impulse(direction*speed*40)
+				hide()
+				queue_free()
+		else:
+			hide()
+			queue_free()
 
 func _on_DecayTimer_timeout():
 	hide()
@@ -42,3 +58,4 @@ func _on_Projectile_body_entered( body ):
 	else:
 		hide()
 		queue_free()
+
