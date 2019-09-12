3b49dbe56412f6de1eb3ab4a12087204ac388524
James <jamoxploder@gmail.com>
2019-09-12 06:46:04 +0200

    More quest work

--- Scripts/Quests/Quest.gd
+++ Scripts/Quests/Quest.gd
@@ -3,7 +3,9 @@ extends Node
 class_name Quest, "res://Scripts/Quests/Quest.gd"
 
 var quest_state # 0 - Inactive | 1 - Active | 2 - Item Collected | 3 - Reward Collected (Complete)
-var quest_loca
+var location_name
+var reward #id of item given as reward
+var difficulty #Scale of 1-5 , 1 being the easiest
 
 func _init(var state):
 	quest_state = state
