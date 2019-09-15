c85d1b8a74ae0bb7bac3009db1591ea4409799cf
James <jamoxploder@gmail.com>
2019-09-15 10:07:34 +0200

    All quests added to quest_data

--- Scripts/Quests/Quest.gd
+++ Scripts/Quests/Quest.gd
@@ -3,7 +3,11 @@ extends Node
 class_name Quest, "res://Scripts/Quests/Quest.gd"
 
 var quest_state # 0 - Inactive | 1 - Active | 2 - Item Collected | 3 - Reward Collected (Complete)
+var id
+var NPC
 var location_name
+var location_direction
+var quest_item
 var reward #id of item given as reward
 var difficulty #Scale of 1-5 , 1 being the easiest
 
